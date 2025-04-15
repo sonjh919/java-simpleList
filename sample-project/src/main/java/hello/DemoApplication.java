ee9098743d29ed35a33fbf62ea35c3e6418c01d3
sonjh919 <kingjh1125@gmail.com>
2025-04-15 13:21:28 +0900

    샘플 코드 작성 및 빌드

---
+++ sample-project/src/main/java/hello/DemoApplication.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
